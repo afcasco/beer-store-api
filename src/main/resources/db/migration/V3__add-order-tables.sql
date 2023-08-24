4d436dca94617fc509c021d4b5d99fcad0710ef2
Alex <7701272+afcasco@users.noreply.github.com>
2023-08-24 19:20:17 +0200

    Renamed flyway script v3

--- src/main/resources/db/migration/V3__add-order-tables.sql
+++ src/main/resources/db/migration/V3__add-order-tables.sql
@@ -4,11 +4,11 @@ drop table if exists beer_order;
 create table beer_order
 (
     id                 varchar(36) not null,
-    created_date       datetime(6) default null,
-    customer_ref       varchar(255)default null,
-    last_modified_date DATETIME(6)default null,
-    version            BIGINTdefault null,
-    customer_id        varchar(36)default null,
+    created_date       datetime(6)  default null,
+    customer_ref       varchar(255) default null,
+    last_modified_date DATETIME(6)  default null,
+    version            BIGINT       default null,
+    customer_id        varchar(36)  default null,
     primary key (id),
     constraint foreign key (customer_id) references customer (id)
 ) ENGINE = InnoDB;
@@ -16,12 +16,12 @@ create table beer_order
 create table beer_order_line
 (
     id                 varchar(36) not null,
-    beer_id            varchar(36)default null,
-    created_date       datetime(6)default null,
-    last_modified_date datetime(6)default null,
-    order_quantity     int default null,
-    quantity_allocated int default null,
-    version            bigint default null,
+    beer_id            varchar(36) default null,
+    created_date       datetime(6) default null,
+    last_modified_date datetime(6) default null,
+    order_quantity     int         default null,
+    quantity_allocated int         default null,
+    version            bigint      default null,
     beer_order_id      varchar(36) default null,
     primary key (id),
     constraint foreign key (beer_order_id) references beer_order (id),
